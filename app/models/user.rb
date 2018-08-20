716474f896f74a9b0e3abedc557e210ff0551f6c
john-raber <jrab224@gmail.com>
2018-08-20 11:45:12 -0400

    adding users#index and #show views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :beers, through: :reviews
   has_many :breweries, through: :beers
 
+  # Uses current day to return a user's age as an integer
   def current_age
     Date.today.year - birthdate.year
   end
