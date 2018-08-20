6f59eb5825e9c1fc15e1312de601193a464a5aa8
john-raber <jrab224@gmail.com>
2018-08-20 11:30:31 -0400

    added users#index and users#show routes defined User#current_age

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :beers, through: :reviews
   has_many :breweries, through: :beers
+
+  def current_age
+    Date.today.year - birthdate.year
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :breweries, only: [:index, :show]
   resources :reviews
   resources :beers,  only: [:index, :show]
-  resources :users
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
