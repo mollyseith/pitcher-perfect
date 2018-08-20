6517d61e94c7314875f1533dfc84536dda12227f
john-raber <jrab224@gmail.com>
2018-08-20 18:11:58 -0400

    adding edit, update, and delete for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -21,6 +21,24 @@ class UsersController < ApplicationController
     else
       redirect_to @user
     end
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+
+    if @user.errors.any?
+      render :edit
+    else
+      redirect_to @user
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
   end
 
   private
