36e6b8fa22fb3f73d7ec993a2f063501ab347528
github username <github email address>
2018-08-20 16:17:12 -0400

    added part of form (not yet complete)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,14 @@
 User.create(name: 'Steve', birthdate: '1990-01-01')
-
-
-Review.create(user_id: 1, beer_id: 1, content: 'This beer sucks', rating: 1)
-
+User.create(name: 'Molly', birthdate: '1993-05-09')
+User.create(name: 'John', birthdate: '1954-12-19')
+User.create(name: 'Ann', birthdate: '1997-03-01')
 
 require 'csv'
-
 CSV.foreach("breweries.utf8.csv", headers: true) {|row| Brewery.create(name: "#{row[1]}")}
 CSV.foreach("beer.utf8.csv", headers: true) {|row| Beer.create(name: "#{row[2]}", brewery_id: "#{row[1]}")}
-# parsed_csv = File.open('beer.csv').readlines.map{|line| line.split(',')}
-# parsed_csv.shift
-#
-# parsed_csv.each do |line|
-#
-#
-#
-#     home_team = Team.find_or_create_by(name: remove_numbers_from(line[2]))
-#     away_team = Team.find_or_create_by(name: remove_numbers_from(line[5]))
-#     home_team_score = line[3].split('-')[0].to_i
-#     away_team_score = line[3].split('-')[1].to_i
-#     # binding.pry
-#     Match.create(home_team_id: home_team.id, away_team_id: away_team.id,
-#         home_team_score: home_team_score, away_team_score: away_team_score)
-# end
+
+Review.create(user_id: 1, beer_id: 1, content: 'This beer sucks', rating: 1)
+Review.create(user_id: 3, beer_id: 22, content: 'This beer good', rating: 2)
+Review.create(user_id: 2, beer_id: 98, content: 'This beer bad', rating: 2)
+Review.create(user_id: 4, beer_id: 42, content: 'This beer ok', rating: 4)
+Review.create(user_id: 4, beer_id: 540, content: 'This beer really bad', rating: 3)
