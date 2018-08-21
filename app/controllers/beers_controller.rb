528dcfd6c3067f2f154b551c861833b8d7752711
john-raber <jrab224@gmail.com>
2018-08-21 11:42:08 -0400

    adding search bar to beers index page

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -2,7 +2,13 @@ class BeersController < ApplicationController
   before_action :set_beer, only: [:show]
 
   def index
-    @beers = Beer.all
+    # If a user has used the search bar (params[:beer_name]), then we will use
+    # their search to limit the beers displayed
+    if params.include?(:beer_name)
+      @beers = Beer.where('name LIKE ?', "%#{params[:beer_name]}%")
+    else
+      @beers = Beer.all
+    end
   end
 
   def show
