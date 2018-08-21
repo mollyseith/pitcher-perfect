08eb2d8de26b4b38777136ae9fd8288f6f856a5c
github username <github email address>
2018-08-21 13:00:29 -0400

    added rating to review form. added buttons to layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@ require 'csv'
 CSV.foreach("breweries.utf8.csv", headers: true) {|row| Brewery.create(name: "#{row[1]}")}
 CSV.foreach("beer.utf8.csv", headers: true) {|row| Beer.create(name: "#{row[2]}", brewery_id: "#{row[1]}")}
 
-Review.create(user_id: 1, beer_id: 1, content: 'This beer sucks', rating: 1)
-Review.create(user_id: 3, beer_id: 22, content: 'This beer good', rating: 2)
-Review.create(user_id: 2, beer_id: 98, content: 'This beer bad', rating: 2)
-Review.create(user_id: 4, beer_id: 42, content: 'This beer ok', rating: 4)
-Review.create(user_id: 4, beer_id: 540, content: 'This beer really bad', rating: 3)
+Review.create(user_id: 1, beer_id: 1, content: 'This beer sucks', rating: 1.5)
+Review.create(user_id: 3, beer_id: 22, content: 'This beer good', rating: 2.5)
+Review.create(user_id: 2, beer_id: 98, content: 'This beer bad', rating: 2.0)
+Review.create(user_id: 4, beer_id: 42, content: 'This beer ok', rating: 4.5)
+Review.create(user_id: 4, beer_id: 540, content: 'This beer really bad', rating: 3.0)
