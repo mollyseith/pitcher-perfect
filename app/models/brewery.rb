be607d6853d59f2401aa8f9d6798aa59818c2a07
john-raber <jrab224@gmail.com>
2018-08-23 08:34:35 -0400

    adding comments for brewery methods

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -3,12 +3,14 @@ class Brewery < ApplicationRecord
   has_many :reviews, through: :beers
   has_many :users, through: :reviews
 
+  # selects all of a brewery's beers which have an associated review
   def reviewed_beers
     beers.select do |beer|
       beer.reviews_count > 0
     end
   end
 
+  # counts the total number of reviews associated with a brewery's beers
   def reviewed_beers_count
     review_count = beers.inject(0) do |total, beer|
       total + beer.reviews_count
@@ -16,6 +18,8 @@ class Brewery < ApplicationRecord
     review_count
   end
 
+  # calculates the average_rating for all of a brewery's beers which have been
+  # reviewed
   def avg_beer_rating
     total = reviewed_beers.inject(0) do |s, b|
       s + b.average_rating
@@ -23,30 +27,41 @@ class Brewery < ApplicationRecord
     total / reviewed_beers.count
   end
 
+  # sorts a brewery's beers by their average_rating
   def order_beers_by_avg_rating
     reviewed_beers.sort_by do |beer|
       beer.average_rating
     end
   end
 
+  # selects the beer with the highest average_rating from the beers ordered by
+  # their average rating
   def best_beer
     order_beers_by_avg_rating[-1]
   end
 
+  # selects the beer with the lowest average_rating from the beers ordered by
+  # their average rating
   def worst_beer
     order_beers_by_avg_rating[0]
   end
 
+  # sorts all breweries according to the average_rating for all of a brewery's
+  # beers
   def self.order_by_avg_beer_rating
     all.sort_by do |b|
       b.avg_beer_rating
     end
   end
 
+  # selects the brewery with the highest average_beer_rating from the breweries
+  # ordered by their average beer rating
   def self.best_brewery
     order_by_avg_beer_rating[-1]
   end
 
+  # selects the brewery with the lowest average_beer_rating from the breweries
+  # ordered by their average beer rating
   def self.worst_brewery
     order_by_avg_beer_rating[0]
   end
