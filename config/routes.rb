fbc1a4555966d4f45fc5794cedca37d2824d0798
john-raber <jrab224@gmail.com>
2018-08-24 14:46:42 -0400

    adding root route and followers/following to users show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
-  # resources :venues
   resources :breweries, only: [:index, :show]
   resources :reviews
   resources :beers,  only: [:index, :show]
   resources :users
   resources :venues
   resources :relationships, only: [:create, :destroy]
+
+  root 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
